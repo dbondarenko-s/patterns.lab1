e657bd59c225f50b1dc5182732e7ea8c5292a66e
Danil Bondarenko <s.dbondarenko@yandex.ru>
2024-10-16 10:37:03 +0300

    Инициализация проекта

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Patterns.Lab1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
